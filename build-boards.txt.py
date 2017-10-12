5539f9aa766bd4bfa8bcd0bdd63b11d7948b1219
david gauchard <gauchard@laas.fr>
2017-10-12 02:34:42 +0200

    wip

--- build-boards.txt.py
+++ build-boards.txt.py
@@ -25,103 +25,197 @@ boards = [
 		'name': 'Generic ESP8266 Module',
 		'opts': {
 			'.build.board': 'ESP8266_ESP01',
+## flash_mode should be defined elsewhere and merged (in 'opts') where appropriate, how do we easily merge in python?
+			'.menu.FlashMode.dio.build.flash_mode': 'dio',
+			'.menu.FlashMode.qio.build.flash_mode': 'qio',
+			'.menu.FlashMode.dout.build.flash_mode': 'dout',
+			'.menu.FlashMode.qout.build.flash_mode': 'qout',
 			}
 	},
 	{
 		'short': 'esp8285',
 		'name': 'Generic ESP8285 Module',
+		'opts': {
+			'.build.board': 'ESP8266_ESP01',
+			'.build.flash_mode': 'dout',
+			}
 	},
 	{
 		'short': 'espduino',
 		'name': 'ESPDuino (ESP-13 Module)',
+		'opts': {
+			'.build.board': 'ESP8266_ESP13',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'huzzah',
 		'name': 'Adafruit HUZZAH ESP8266',
+		'opts': {
+			'.build.board': 'ESP8266_ESP12',
+			}
 	},
 	{
 		'short': 'espresso_lite_v1',
 		'name': 'ESPresso Lite 1.0',
+		'opts': {
+			'.build.board': 'ESP8266_ESPRESSO_LITE_V1',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'espresso_lite_v2',
 		'name': 'ESPresso Lite 2.0',
+		'opts': {
+			'.build.board': 'ESP8266_ESPRESSO_LITE_V2',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'phoenix_v1',
 		'name': 'Phoenix 1.0',
+		'opts': {
+			'.build.board': 'ESP8266_PHOENIX_V1',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'phoenix_v2',
 		'name': 'Phoenix 2.0',
+		'opts': {
+			'.build.board': 'ESP8266_PHOENIX_V2',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'nodemcu',
 		'name': 'NodeMCU 0.9 (ESP-12 Module)',
+		'opts': {
+			'.build.board': 'ESP8266_NODEMCU',
+			}
 	},
 	{
 		'short': 'nodemcuv2',
 		'name': 'NodeMCU 1.0 (ESP-12E Module)',
+		'opts': {
+			'.build.board': 'ESP8266_NODEMCU',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'modwifi',
 		'name': 'Olimex MOD-WIFI-ESP8266(-DEV)',
+		'opts': {
+			'.build.board': 'MOD_WIFI_ESP8266',
+			}
 	},
 	{
 		'short': 'thing',
 		'name': 'SparkFun ESP8266 Thing',
+		'opts': {
+			'.build.board': 'ESP8266_THING',
+			}
 	},
 	{
 		'short': 'thingdev',
 		'name': 'SparkFun ESP8266 Thing Dev',
+		'opts': {
+			'.build.board': 'ESP8266_THING_DEV',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'esp210',
 		'name': 'SweetPea ESP-210',
-	},
-	{
-		'short': '# wifio',
-		'name': 'Wifio',
+		'opts': {
+			'.build.board': 'ESP8266_ESP210',
+			}
 	},
 	{
 		'short': 'd1_mini',
 		'name': 'WeMos D1 R2 & mini',
+		'opts': {
+			'.build.board': 'ESP8266_WEMOS_D1R2MINI',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'd1_mini_lite',
 		'name': 'Wemos D1 mini lite (ESP8285)',
+		'opts': {
+			'.build.board': 'ESP8266_WEMOS_D1MINILITE',
+			'.build.flash_mode': 'dout',
+			}
 	},
 	{
 		'short': 'd1',
-		'name': 'WeMos D1',
+		'name': 'WeMos D1 R1',
+		'opts': {
+			'.build.board': 'ESP8266_WEMOS_D1R1',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	{
 		'short': 'espino',
 		'name': 'ESPino (ESP-12 Module)',
+		'opts': {
+			'.build.board': 'ESP8266_ESP12',
+			'.menu.FlashMode.dio.build.flash_mode': 'dio',
+			'.menu.FlashMode.qio.build.flash_mode': 'qio',
+			}
 	},
 	{
 		'short': 'espinotee',
 		'name': 'ThaiEasyElec\'s ESPino',
+		'opts': {
+			'.build.board': 'ESP8266_ESP13',
+			}
 	},
 	{
 		'short': 'wifinfo',
 		'name': 'WifInfo',
+		'opts': {
+			'.build.board': 'WIFINFO',
+			'.menu.ESPModule.ESP12.build.board': 'ESP8266_ESP12',
+			'.menu.FlashMode.dio.build.flash_mode': 'dio',
+			'.menu.FlashMode.qio.build.flash_mode': 'qio',
+			}
 	},
 	{
 		'short': 'coredev',
 		'name': 'Core Development Module',
+		'opts': {
+			'.build.board': 'ESP8266_ESP01',
+			'.menu.FlashMode.dio.build.flash_mode': 'dio',
+			'.menu.FlashMode.qio.build.flash_mode': 'qio',
+			'.menu.FlashMode.dout.build.flash_mode': 'dout',
+			'.menu.FlashMode.qout.build.flash_mode': 'qout',
+			}
 	},
 	{
 		'short': 'arduino-esp8266',
 		'name': 'Arduino',
+		'opts': {
+			'.build.board': 'ESP8266_ARDUINO',
+			'.menu.BoardModel.starottodeved.build.board': 'ESP8266_ARDUINO_STAR_OTTO',
+			'.menu.BoardModel.primo.build.board': 'ESP8266_ARDUINO_PRIMO',
+			'.menu.BoardModel.unowifideved.build.board': 'ESP8266_ARDUINO_UNOWIFI',
+			}
 	},
 	{
 		'short': 'gen4iod',
 		'name': '4D Systems gen4 IoD Range',
+		'opts': {
+			'.build.board': 'GEN4_IOD',
+			}
 	},
 	{
 		'short': 'oak',
 		'name': 'DigiStump Oak',
+		'opts': {
+			'.build.board': 'ESP8266_OAK',
+			'.build.flash_mode': 'dio',
+			}
 	},
 	]
 
